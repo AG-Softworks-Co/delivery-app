7626b186065747c0c4adeefc943da9f4c0bbf6bb
Verzach3 <gabrielcorreacardenas@gmail.com>
2025-01-27 10:29:49 -0500

    Add android project

---
+++ frontend/android/app/src/main/java/com/agsoftworks/delivery/dev/MainActivity.java
@@ -0,0 +1,5 @@
+package com.agsoftworks.delivery.dev;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.agsoftworks.delivery.dev',
+  appName: 'Delivery',
+  webDir: 'dist'
+};
+
+export default config;
